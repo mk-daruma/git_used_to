f4e7845170a06144347dd3519eaa11aadc2e9ffe
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-07-20 17:11:10 +0900

    [add]quiz_commentのテーブルを作成
    問題に対して、コメントができる機能のために作成。

---
+++ db/migrate/20220720081016_create_quiz_comments.rb
@@ -0,0 +1,11 @@
+class CreateQuizComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :quiz_comments do |t|
+      t.text :quiz_comment
+      t.references :quiz, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
